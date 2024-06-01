c07ee7c1f3fcacebda6a233f5cb97ae6d7f4a2d3
Imtiaz Mahmood <imtiazmehmood26@gmail.com>
2024-06-01 13:31:32 +0500

    setup cart and payment success screens

---
+++ lib/actions/actions.ts
@@ -0,0 +1,44 @@
+export const getCollections = async () => {
+  const collections = await fetch(`${process.env.NEXT_BASE_URL}/collections`);
+  return await collections.json();
+};
+
+export const getCollectionDetails = async (collectionId: string) => {
+  const collection = await fetch(
+    `${process.env.NEXT_BASE_URL}/collections/${collectionId}`
+  );
+  return await collection.json();
+};
+
+export const getProducts = async () => {
+  const products = await fetch(`${process.env.NEXT_BASE_URL}/products`);
+  return await products.json();
+};
+
+export const getProductDetails = async (productId: string) => {
+  const product = await fetch(
+    `${process.env.NEXT_BASE_URL}/products/${productId}`
+  );
+  return await product.json();
+};
+
+export const getSearchedProducts = async (query: string) => {
+  const searchedProducts = await fetch(
+    `${process.env.NEXT_BASE_URL}/search/${query}`
+  );
+  return await searchedProducts.json();
+};
+
+export const getOrders = async (customerId: string) => {
+  const orders = await fetch(
+    `${process.env.NEXT_BASE_URL}/orders/customers/${customerId}`
+  );
+  return await orders.json();
+};
+
+export const getRelatedProducts = async (productId: string) => {
+  const relatedProducts = await fetch(
+    `${process.env.NEXT_BASE_URL}/products/${productId}/related`
+  );
+  return await relatedProducts.json();
+};
