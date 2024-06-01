08ebcb437703d5bc7da3d47f33698349a2550443
Imtiaz Mahmood <imtiazmehmood26@gmail.com>
2024-06-01 13:10:57 +0500

    Initial comment

---
+++ tailwind.config.ts
@@ -0,0 +1,100 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    fontSize: {
+      "heading1-bold": [
+        "50px",
+        {
+          lineHeight: "100%",
+          fontWeight: "700",
+        },
+      ],
+      "heading2-bold": [
+        "30px",
+        {
+          lineHeight: "100%",
+          fontWeight: "700",
+        },
+      ],
+      "heading3-bold": [
+        "24px",
+        {
+          lineHeight: "100%",
+          fontWeight: "700",
+        },
+      ],
+      "heading4-bold": [
+        "20px",
+        {
+          lineHeight: "100%",
+          fontWeight: "700",
+        },
+      ],
+      "body-bold": [
+        "18px",
+        {
+          lineHeight: "100%",
+          fontWeight: "700",
+        },
+      ],
+      "body-semibold": [
+        "18px",
+        {
+          lineHeight: "100%",
+          fontWeight: "600",
+        },
+      ],
+      "body-medium": [
+        "18px",
+        {
+          lineHeight: "100%",
+          fontWeight: "500",
+        },
+      ],
+      "base-bold": [
+        "16px",
+        {
+          lineHeight: "100%",
+          fontWeight: "600",
+        },
+      ],
+      "base-medium": [
+        "16px",
+        {
+          lineHeight: "100%",
+          fontWeight: "500",
+        },
+      ],
+      "small-bold": [
+        "14px",
+        {
+          lineHeight: "140%",
+          fontWeight: "700",
+        },
+      ],
+      "small-medium": [
+        "14px",
+        {
+          lineHeight: "140%",
+          fontWeight: "500",
+        },
+      ],
+    },
+    extend: {
+      colors: {
+        "red-1": "#FF0000",
+        "grey-1": "#F7F7F7",
+        "grey-2": "#8A8A8A",
+      },
+    },
+  },
+  plugins: [require("tailwind-scrollbar-hide")],
+};
+export default config;
+
