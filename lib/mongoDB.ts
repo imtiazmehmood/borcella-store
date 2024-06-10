ad0b31a9418b83d8d4ae63abc0f4fd4f8a1a22cc
Imtiaz Mahmood <imtiazmehmood26@gmail.com>
2024-06-10 18:32:34 +0500

    update db

--- lib/mongoDB.ts
+++ lib/mongoDB.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 let isConnected: boolean = false;
 
 export const connectToDB = async (): Promise<void> => {
-  mongoose.set("strictQuery", true)
+  mongoose.set("strictQuery", true);
 
   if (isConnected) {
     console.log("MongoDB is already connected");
@@ -12,12 +12,12 @@ export const connectToDB = async (): Promise<void> => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URL || "", {
-      dbName: "Borcelle_Store"
-    })
+      dbName: "Borcelle_Admin",
+    });
 
     isConnected = true;
     console.log("MongoDB is connected");
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
-}
+};
